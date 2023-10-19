ab3316ff81ed7e790e58434c595aac39bf9b3b03
Lucas Viana <lucasviana0604@gmail.com>
2023-10-19 18:18:54 -0300

    Will be using menhir and ocamllex for parsing

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,14 +1,20 @@
-type id = string
-type binop = Plus | Minus | Times | Div
+module Ast = struct
+  type id = string
 
-type stm =
-  | CompoundStm of stm * stm
-  | AssignStm of id * exp
-  | PrintStm of exp list
+  type stm =
+    | CompoundStm of stm * stm
+    | AssignStm of id * exp
+    | PrintStm of exp list
 
-and exp =
-  | IdExp of id
-  | NumExp of int
-  | OpExp of exp * binop * exp
-  | EseqExp of stm * exp
+  and exp =
+    | IdExp of id
+    | NumExp of int
+    | OpExp of exp * binop * exp
+    | EseqExp of stm * exp
 
+  and binop =
+    | Plus of exp * exp
+    | Minus of exp * exp
+    | Times of exp * exp
+    | Div of exp * exp
+end
---
+++ lib/parser.mly
@@ -0,0 +1,46 @@
+%token TYPE
+%token VAR
+%token FUNCION
+%token BREAK
+%token OF
+%token END
+%token IN
+%token NIL
+%token LET
+%token DO
+%token TO
+%token FOR
+%token WHILE
+%token ELSE
+%token THEN
+%token IF
+%token ARRAY
+%token ASSIGN
+%token OR
+%token AND
+%token GE
+%token GT
+%token LE
+%token LT
+%token NEQ
+%token EQ
+%token DIVIDE
+%token TIMES
+%token MINUS
+%token PLUS
+%token DOT
+%token RBRACE
+%token LBRACE
+%token RBRACK
+%token LBRACK
+%token RPAREN
+%token LPAREN
+%token SEMICOLON
+%token COLON
+%token COMMA
+%token <string> STRING
+%token <int> INT
+%token <string> ID
+%token EOF
+
+%%
